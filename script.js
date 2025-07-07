b1894efbd1ebd0f6cc87dca1f2ebe689d9a3e33a
Erick Silva <166569920+erickao00@users.noreply.github.com>
2025-07-07 15:07:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+
+document.getElementById('botaoMensagem').addEventListener('click', function () {
+    document.getElementById('mensagem').innerText = "Você é capaz de grandes coisas apenas acredite em você 🚀";
+});
